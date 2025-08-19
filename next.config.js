21c2ecf6ef37014b0ce70f788cdce29755fd753f
pvsameerpvs <pvsameerpvs@gmail.com>
2025-08-19 15:05:38 +0400

    Updates

--- next.config.js
+++
@@ -1,8 +0,0 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  output: "export",
-  basePath: "/sneaker_hub",
-  assetPrefix: "/sneaker_hub/",
-  images: { unoptimized: true }, // <- important for static export
-};
-module.exports = nextConfig;
